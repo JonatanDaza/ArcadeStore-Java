4023a4d62576c4ffb6d6977f28838053b4b61a1b
JonatanDaza <j0n4t4n.d424@gmail.com>
2025-06-14 20:04:53 -0500

    first commit

---
+++ ArcadeStore/src/main/java/com/Scrum3/ArcadeStore/ArcadeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.Scrum3.ArcadeStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcadeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcadeStoreApplication.class, args);
+	}
+
+}
---
+++ ArcadeStore/src/test/java/com/Scrum3/ArcadeStore/ArcadeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scrum3.ArcadeStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcadeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
